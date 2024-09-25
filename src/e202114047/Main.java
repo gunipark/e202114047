c826b36025cbd4293f7026b87124ac8b03035b2e
Park EunSol <eunsolguni1353@gmail.com>
2024-09-25 09:57:45 +0900

    fourth

--- src/e202114047/Main.java
+++ src/e202114047/Main.java
@@ -3,9 +3,11 @@ package e202114047;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("박은솔");
-    World.run();
-  }
+	    System.out.println("박은솔");
+	    World.run();
+	    Hello.run();
+	  }
+
 
 }
 
